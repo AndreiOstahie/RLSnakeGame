2f1d2d06bcf1103b5cae7cd0f96401a5ccf196a1
Andrei <andreiostahie@yahoo.com>
2024-03-09 20:33:30 +0200

    Snake game; agent; model; plot doesn't work; AI needs some more work

---
+++ RLSnakeGame/agent.py
@@ -0,0 +1,156 @@
+import torch
+import random
+import numpy as np
+from snakeAI import SnakeGameAI, Point, Direction, BLOCK_SIZE
+from collections import deque
+from model import Linear_Qnet, QTrainer
+from plothelper import plot
+
+MAX_MEMORY = 100_000 # deque max memory size
+BATCH_SIZE = 1000 # memory sample (min) size
+LR = 0.001 # learning rate
+
+
+class Agent:
+    def __init__(self):
+        self.n_games = 0  # number of games
+        self.epsilon = 0  # randomness param
+        self.gamma = 0.9  # discount rate (val < 1 - modify val for different results)
+        self.memory = deque(maxlen=MAX_MEMORY)  # removes elements by using popleft() when max length is exceeded
+
+        self.model = Linear_Qnet(11, 256, 3)  # 11 possible states, 3 possible actions, 256 - modify value for different results
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
+
+
+
+    def get_state(self, game):
+        # 11 Possible states:
+        # [ danger_forward, danger_right, danger_left,
+        #   dir_left, dir_right, dir_up, dir_down,
+        #   food_left, food_right, food_up, food_down ]
+        # -- 0 if false, 1 if true --
+
+        head = game.snake[0]
+        point_left = Point(head.x - BLOCK_SIZE, head.y)
+        point_right = Point(head.x + BLOCK_SIZE, head.y)
+        point_up = Point(head.x, head.y - BLOCK_SIZE)
+        point_down = Point(head.x, head.y + BLOCK_SIZE)
+
+        dir_left = game.direction == Direction.LEFT
+        dir_right = game.direction == Direction.RIGHT
+        dir_up = game.direction == Direction.UP
+        dir_down = game.direction == Direction.DOWN
+
+        state = [
+            # danger_forward
+            (dir_right and game.is_collision(point_right)) or
+            (dir_left and game.is_collision(point_left)) or
+            (dir_up and game.is_collision(point_up)) or
+            (dir_down and game.is_collision(point_down)),
+
+            # danger_right
+            (dir_right and game.is_collision(point_down)) or
+            (dir_left and game.is_collision(point_up)) or
+            (dir_up and game.is_collision(point_right)) or
+            (dir_down and game.is_collision(point_left)),
+
+            # danger_left
+            (dir_right and game.is_collision(point_up)) or
+            (dir_left and game.is_collision(point_down)) or
+            (dir_up and game.is_collision(point_left)) or
+            (dir_down and game.is_collision(point_right)),
+
+            # move direction
+            dir_left,
+            dir_right,
+            dir_up,
+            dir_down,
+
+            # food position
+            game.food.x < game.head.x,  # food left
+            game.food.x > game.head.x,  # food right
+            game.food.y < game.head.y,  # food up
+            game.food.y > game.head.y  # food down
+        ]
+
+        return np.array(state, dtype=int)
+
+
+
+    def remember(self, state, action, reward, next_state, game_over):
+        self.memory.append((state, action, reward, next_state, game_over))
+
+    def train_long_memory(self):
+        if len(self.memory) > BATCH_SIZE:
+            memory_sample = random.sample(self.memory, BATCH_SIZE)  # list of tuples
+        else:
+            memory_sample = self.memory
+
+        sample_states, sample_actions, sample_rewards, sample_next_states, sample_game_overs = zip(*memory_sample)  # extract and group items of the same type together
+        self.trainer.train_step(sample_states, sample_actions, sample_rewards, sample_next_states, sample_game_overs)
+
+    def train_short_memory(self, state, action, reward, next_state, game_over):
+        self.trainer.train_step(state, action, reward, next_state, game_over)
+
+    def get_action(self, state):
+        # random moves: tradeoff between exploration and exploitation
+        self. epsilon = 80 - self.n_games
+        action = [0, 0, 0]
+        if random.randint(0, 200) < self.epsilon:
+            # perform random action
+            move = random.randint(0, 2)
+            action[move] = 1
+        else:
+            # perform action based on the model
+            state_tensor = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state_tensor)  # predict next action
+            move = torch.argmax(prediction).item()  # get max argument from prediction and convert it from tensor to item
+            action[move] = 1
+
+        return action
+
+def train():
+    plot_scores = []
+    plot_avg_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+
+    while True:
+        # get current state
+        state = agent.get_state(game)
+
+        # get action
+        action = agent.get_action(state)
+
+        # perform move and get new state
+        reward, game_over, score = game.play_step(action)
+        state_new = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(state, action, reward, state_new, game_over)
+
+        # remember
+        agent.remember(state, action, reward, state_new, game_over)
+
+        if game_over:
+            # train long memory, plot results
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                agent.model.save()
+
+            print('Game', agent.n_games, 'Score', score, 'Record', record)
+
+            plot_scores.append(score)
+            total_score += score
+            avg_score = total_score / agent.n_games
+            plot_avg_scores.append(avg_score)
+            plot(plot_scores, plot_avg_scores)
+
+if __name__ == '__main__':
+    train()
---
+++ RLSnakeGame/model.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as nnFunc
+import torch.optim as optim
+import os
+
+class Linear_Qnet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):  # feed-forward neural network with 3 layers: input, hidden, output
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
+
+    # "prediction" function
+    def forward(self, x):
+        x = nnFunc.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
+
+    def save(self, file_name='model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+
+class QTrainer:
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr)
+        self.criterion = nn.MSELoss()  # loss function -> mean squared error
+
+    def train_step(self, state, action, reward, next_state, game_over):
+        # data is in correct shape -> (n, x)
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+
+
+
+        if len(state.shape) == 1:  # train model with a single instance (train_short_memory in agent)
+            # data is not in correct shape -> convert data from format x to format (1, x)
+            state = torch.unsqueeze(state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            game_over = (game_over, )  # tuple with single value
+
+        # predicted Q values with current state
+        prediction = self.model(state)
+
+        target = prediction.clone()
+        for i in range(len(game_over)):
+            q_new = reward[i]
+            if not game_over[i]:
+                q_new = reward[i] + self.gamma * torch.max(self.model(next_state[i]))
+
+            target[i][torch.argmax(action).item()] = q_new
+
+        # predict new Q value: Q_new = r + y * max(next_predicted_Q_value) -> only do this if not game_over
+        # prediction.clone
+        # predictionss[argmax(action)] = Q_new
+
+        self.optimizer.zero_grad()  # empty gradient
+        loss = self.criterion(target, prediction)
+        loss.backward()
+
+        self.optimizer.step()
+
